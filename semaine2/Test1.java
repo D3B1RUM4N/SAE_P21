ce203efe988263e3a2cbfb439135750069bc9a20
mekhinini <elies.mekhinini@etu.unistra.fr>
2022-05-09 11:55:01 +0200

    lecture d'un .txt

--- semaine2/Test1.java
+++ semaine2/Test1.java
@@ -1,3 +1,27 @@
+import java.io.File;
+import java.io.FileInputStream;
+
+
 public class Test1 {
 
+    //essai de l'ecture d'un texte en .txt
+    public static void main(String args[]){
+
+        //lecture du fichier
+        try
+        {
+            File f = new File("../fichierTXT/txt1.txt");
+            FileInputStream fIS = new FileInputStream(f);
+            System.out.println("file content: ");
+            int r = 0;
+            while((r = fIS.read())!=-1)
+            {
+                System.out.print((char)r);
+            }
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
 }
