992175e2963750e461ee7519ff25e4fd1b5a99d6
mekhinini <elies.mekhinini@etu.unistra.fr>
2022-05-09 12:05:03 +0200

    essaie lecture d'un .class, echeque

---
+++ semaine2/Test2.java
@@ -0,0 +1,28 @@
+import java.io.File;
+import java.io.FileInputStream;
+
+
+public class Test2 {
+
+    //essai de l'ecture d'un texte en .class
+    public static void main(String args[]){
+
+        //lecture du fichier
+        try
+        {
+            File f = new File("../fichierTXT/GType.class");
+            FileInputStream fIS = new FileInputStream(f);
+            System.out.println("file content: ");
+            int r = 0;
+            while((r = fIS.read())!=-1)
+            {
+                System.out.print((char)r);
+            }
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+        //resultat etrange
+    }
+}
