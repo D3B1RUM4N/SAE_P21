371da41bf7b897083d2dc3a6424d6f2ef8b6f154
mekhinini <elies.mekhinini@etu.unistra.fr>
2022-05-16 12:06:05 +0200

    lecture des mathode et des attribute

--- semaine2/semaineDeux.java
+++ semaine2/semaineDeux.java
@@ -1,3 +1,6 @@
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -8,6 +11,7 @@ public class semaineDeux {
 
     public static String stringClass;
     public static Class cName;
+    public static String fichier = "semaineDeux";
 
 
     public static void main(String[] args)
@@ -15,6 +19,8 @@ public class semaineDeux {
         //recuperation de la class
         classGet();
         umlClass();
+        umlAtribute();
+        umlMethode();
 
         //fermeture de la class
         stringClass += lineSeparator() +
@@ -28,7 +34,7 @@ public class semaineDeux {
     {
 
         try {
-            cName = Class.forName("semaineDeux");
+            cName = Class.forName(fichier);
         }
         catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
@@ -49,6 +55,40 @@ public class semaineDeux {
 
 
         stringClass += "class " + cName.getName() + "{" + lineSeparator();
+    }
+
+    public static void umlAtribute()
+    {
+        try {
+            Field[] attribut = cName.getDeclaredFields();
+            for(Field val : attribut)
+            {
+                stringClass += val.getName() + lineSeparator();
+            }
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void umlMethode()
+    {
+        int vis;
+        try {
+            Method[] methode = cName.getDeclaredMethods();
+            for(Method val : methode)
+            {
+                //vis = val.getModifiers();
+                //System.out.println("///////////////////////" + vis);
+
+                stringClass += val.getName() + "()" + lineSeparator();
+            }
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        //System.out.println("La classe de l'objet obj est : " + c.getName())
     }
 
     public static void ecriture()
